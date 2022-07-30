b3ce9172d137f1c39fa7baf9ef3f38443353e9e0
edpe2003 <edpe2003@gmail.com>
2022-07-30 19:41:07 -0300

    Finish register page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,9 @@ import users from "./users";
 import roles from "./roles";
 import auth from "./auth";
 
+const cors = require('cors');
 function router(app: Express) {
+    app.use(cors())
     const router: Router = express.Router()
     app.use("/api/v1", router);
     router.use("/users", users);
